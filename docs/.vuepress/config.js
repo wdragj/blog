5d8957397fca88a2b87431e5103a0dc2e33c578e
Freddy <wdragj@gmail.com>
2023-02-16 13:58:54 -0600

    plugins

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,16 @@
 module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
-    base: '/blog/'
+    base: '/blog/',
+    plugins: [
+        '@vuepress/active-header-links', {
+        sidebarLinkSelector: '.sidebar-link',
+        headerAnchorSelector: '.header-anchor'
+        }, '@vuepress/back-to-top',
+        '@vuepress/last-updated',
+        '@vuepress/google-analytics',
+        {
+            'ga': '' // UA-00000000-0
+        }
+    ]
 }
